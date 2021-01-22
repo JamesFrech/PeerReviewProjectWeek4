edd06ad3dfaecdd2babb13a77e010ea62109f587
James Frech <jamesjfrech@gmail.com>
2021-01-21 20:17:04 -0500

    changed addresses for read_csv's

--- tests/PeerReview4FunctionTests.R
+++ tests/PeerReview4FunctionTests.R
@@ -1,9 +1,9 @@
 library(readr)
 library(testthat)
 
-data.frame2013 <- readr::read_csv("accident_2013.csv.bz2")
-data.frame2014 <- readr::read_csv("accident_2014.csv.bz2")
-data.frame2015 <- readr::read_csv("accident_2015.csv.bz2")
+data.frame2013 <- readr::read_csv("C:/Users/james/Desktop/PeerReviewProjectWeek4/accident_2013.csv.bz2")
+data.frame2014 <- readr::read_csv("C:/Users/james/Desktop/PeerReviewProjectWeek4/accident_2014.csv.bz2")
+data.frame2015 <- readr::read_csv("C:/Users/james/Desktop/PeerReviewProjectWeek4/accident_2015.csv.bz2")
 
 
 testthat::expect_that(make_filename(2013), is_identical_to("accident_2013.csv.bz2"))
